9f6f87974f2ea714b5b39b236612f7d2f00e26e2
SanqDuonq <namsang0902s@gmail.com>
2024-12-04 11:21:05 +0700

    Validate Zod

---
+++ schema/user.schema.ts
@@ -0,0 +1,28 @@
+import {object, string, z} from 'zod'
+
+export const createUserSchema = object({
+    body: object({
+        firstName: string({
+            required_error: 'First name is required'
+        }),
+        lastName: string({
+            required_error: 'Last name is required'
+        }),
+        password: string({
+            required_error: 'Password is required'
+        })
+        .min(8,{message: 'Password is too short - should be min 8 character(s)'}),
+        confirmPassword: string({
+            required_error: 'ConfirmPassword is required'
+        }),
+        email: string({
+            required_error: 'Email is required'
+        })
+        .email({message: 'Email is not valid'})
+    }).refine((data) => data.password === data.confirmPassword, {
+        message: 'Password do not match',
+        path: ['confirmPassword']
+    })
+})
+
+export type CreateUserInput = z.infer<typeof createUserSchema>['body']
